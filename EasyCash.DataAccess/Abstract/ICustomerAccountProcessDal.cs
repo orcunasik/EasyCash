cbf713da69980f8be0510ff88a4e0bf300d17d66
Orçun Aşık <asikorcun@gmail.com>
2023-05-10 19:29:37 +0300

    Created was CustomerAccountProcess interface

---
+++ EasyCash.DataAccess/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,8 @@
+﻿using EasyCash.Entities.Concrete;
+
+namespace EasyCash.DataAccess.Abstract
+{
+    public interface ICustomerAccountProcessDal : IEntityRepositoryDal<CustomerAccountProcess>
+    {
+    }
+}
