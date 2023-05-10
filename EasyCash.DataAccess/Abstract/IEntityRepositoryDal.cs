0fc72e53aef6bc22f90e143d7238872611b81e80
Orçun Aşık <asikorcun@gmail.com>
2023-05-10 19:28:11 +0300

    GenericRepository struct created

---
+++ EasyCash.DataAccess/Abstract/IEntityRepositoryDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace EasyCash.DataAccess.Abstract
+{
+    public interface IEntityRepositoryDal<T> where T : class,new()
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        IList<T> GetList(Expression<Func<T,bool>> filter = null);
+    }
+}
---
+++ EasyCash.DataAccess/Repositories/EfEntityRepositoryDal.cs
@@ -0,0 +1,44 @@
+﻿using EasyCash.DataAccess.Abstract;
+using EasyCash.DataAccess.Concrete;
+using System.Linq.Expressions;
+
+namespace EasyCash.DataAccess.Repositories
+{
+    public class EfEntityRepositoryDal<TEntity> : IEntityRepositoryDal<TEntity> where TEntity : class, new()
+    {
+        public void Delete(TEntity entity)
+        {
+            using EasyCashDbContext context = new EasyCashDbContext();
+            context.Set<TEntity>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public TEntity GetById(int id)
+        {
+            using EasyCashDbContext context = new EasyCashDbContext();
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public IList<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using EasyCashDbContext context = new EasyCashDbContext();
+            return filter == null
+                ? context.Set<TEntity>().ToList()
+                : context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            using EasyCashDbContext context = new EasyCashDbContext();
+            context.Set<TEntity>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using EasyCashDbContext context = new EasyCashDbContext();
+            context.Set<TEntity>().Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
