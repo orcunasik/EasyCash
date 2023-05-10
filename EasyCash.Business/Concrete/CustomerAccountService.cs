f4cc0e21377640de211df8415aa4ccd3a5325b58
Orçun Aşık <asikorcun@gmail.com>
2023-05-10 19:50:02 +0300

    Interface and implementation have been created for the CustomerAccount service.

---
+++ EasyCash.Business/Concrete/CustomerAccountService.cs
@@ -0,0 +1,41 @@
+﻿using EasyCash.Business.Abstract;
+using EasyCash.DataAccess.Abstract;
+using EasyCash.Entities.Concrete;
+
+namespace EasyCash.Business.Concrete
+{
+    public class CustomerAccountService : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal customerAccountDal;
+
+        public CustomerAccountService(ICustomerAccountDal customerAccountDal)
+        {
+            this.customerAccountDal = customerAccountDal;
+        }
+
+        public void Delete(CustomerAccount entity)
+        {
+            customerAccountDal.Delete(entity);
+        }
+
+        public CustomerAccount GetById(int id)
+        {
+            return customerAccountDal.GetById(id);
+        }
+
+        public IList<CustomerAccount> GetList()
+        {
+            return customerAccountDal.GetList();
+        }
+
+        public void Insert(CustomerAccount entity)
+        {
+            customerAccountDal.Insert(entity);
+        }
+
+        public void Update(CustomerAccount entity)
+        {
+            customerAccountDal.Update(entity);
+        }
+    }
+}
