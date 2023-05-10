ff62a6c9006da239d99ee0799e76724e19244b85
Orçun Aşık <asikorcun@gmail.com>
2023-05-10 19:48:54 +0300

    IBaseService named interface was created for basic CRUD operations.

---
+++ EasyCash.Business/Abstract/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace EasyCash.Business.Abstract
+{
+    public interface IBaseService<T> where T : class,new()
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        IList<T> GetList();
+    }
+}
