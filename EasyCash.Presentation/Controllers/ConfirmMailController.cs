ce87992deffd1ecf949ca7121ed5f8e7e258813b
Orçun Aşık <asikorcun@gmail.com>
2023-06-03 18:11:06 +0300

    Added confirmmail controller

---
+++ EasyCash.Presentation/Controllers/ConfirmMailController.cs
@@ -0,0 +1,36 @@
+﻿using EasyCash.Entities.Concrete;
+using EasyCash.Presentation.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCash.Presentation.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+        private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
+        {
+            AppUser user = await _userManager.FindByEmailAsync(confirmMailViewModel.Email);
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                user.EmailConfirmed = true;
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
