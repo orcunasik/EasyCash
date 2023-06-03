3d7b4d0dc62e8ed4a9d5bc642e2f443e6a25cf19
Orçun Aşık <asikorcun@gmail.com>
2023-06-03 18:11:19 +0300

    Added login controller

---
+++ EasyCash.Presentation/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using EasyCash.Entities.Concrete;
+using EasyCash.Presentation.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCash.Presentation.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password,false,true);
+            if (result.Succeeded)
+            {
+                AppUser user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+                if(user.EmailConfirmed is true)
+                    return RedirectToAction("Index", "MyProfile");
+
+            }
+            return View();
+        }
+    }
+}
