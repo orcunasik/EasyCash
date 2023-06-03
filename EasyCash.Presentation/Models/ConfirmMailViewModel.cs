0e5b058634106c5a89f96fa84a7a88008abf7a73
Orçun Aşık <asikorcun@gmail.com>
2023-06-03 18:11:55 +0300

    Added confirmmail viewmodel

---
+++ EasyCash.Presentation/Models/ConfirmMailViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyCash.Presentation.Models
+{
+    public class ConfirmMailViewModel
+    {
+        public string Email { get; set; }
+        public int ConfirmCode { get; set; }
+    }
+}
