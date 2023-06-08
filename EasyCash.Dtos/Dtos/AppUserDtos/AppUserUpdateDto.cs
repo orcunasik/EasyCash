feb4a570c7a64f1e98ea9152845832745344442f
Orçun Aşık <asikorcun@gmail.com>
2023-06-08 17:23:04 +0300

    Created dto for the logged in Customer to update their profile information.

---
+++ EasyCash.Dtos/Dtos/AppUserDtos/AppUserUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyCash.Dtos.Dtos.AppUserDtos
+{
+    public class AppUserUpdateDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
