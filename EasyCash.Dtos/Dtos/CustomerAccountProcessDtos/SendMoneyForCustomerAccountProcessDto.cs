fdeb86b05d72580fec55cfee9aec9a0867ffa613
Orçun Aşık <asikorcun@gmail.com>
2023-06-12 16:02:28 +0300

    Created dto for SendMoney transaction.

---
+++ EasyCash.Dtos/Dtos/CustomerAccountProcessDtos/SendMoneyForCustomerAccountProcessDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyCash.Dtos.Dtos.CustomerAccountProcessDtos
+{
+    public class SendMoneyForCustomerAccountProcessDto
+    {
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string ReceiverAccountNumber { get; set; }
+        public string SenderAccountNumber { get; set; }
+    }
+}
