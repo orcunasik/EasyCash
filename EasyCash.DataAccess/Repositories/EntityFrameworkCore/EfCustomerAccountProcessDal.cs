ec01bd8ca7f04de22196f1b50d4e11d3f02416dc
Orçun Aşık <asikorcun@gmail.com>
2023-06-12 16:00:38 +0300

    A concrete class has been created to implement the ICustomerAccountProcessDal interface.

---
+++ EasyCash.DataAccess/Repositories/EntityFrameworkCore/EfCustomerAccountProcessDal.cs
@@ -0,0 +1,9 @@
+﻿using EasyCash.DataAccess.Abstract;
+using EasyCash.Entities.Concrete;
+
+namespace EasyCash.DataAccess.Repositories.EntityFrameworkCore
+{
+    public class EfCustomerAccountProcessDal : EfEntityRepositoryDal<CustomerAccountProcess>,ICustomerAccountProcessDal
+    {
+    }
+}
