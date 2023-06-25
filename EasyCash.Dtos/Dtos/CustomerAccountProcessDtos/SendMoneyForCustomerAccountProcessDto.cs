58c453f731be66cb058fbc0d0270fffb419dfb76
Orçun Aşık <asikorcun@gmail.com>
2023-06-25 20:26:45 +0300

    Added Description prop and removed SenderAccountNumber prop.

--- EasyCash.Dtos/Dtos/CustomerAccountProcessDtos/SendMoneyForCustomerAccountProcessDto.cs
+++ EasyCash.Dtos/Dtos/CustomerAccountProcessDtos/SendMoneyForCustomerAccountProcessDto.cs
@@ -5,9 +5,9 @@
         public string ProcessType { get; set; }
         public decimal Amount { get; set; }
         public DateTime ProcessDate { get; set; }
+        public string Description { get; set; }
         public int SenderId { get; set; }
         public int ReceiverId { get; set; }
         public string ReceiverAccountNumber { get; set; }
-        public string SenderAccountNumber { get; set; }
     }
 }
