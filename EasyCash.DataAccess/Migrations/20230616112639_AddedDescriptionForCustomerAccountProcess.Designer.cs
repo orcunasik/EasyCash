f9dbc51b9f3cc3d795904be030a0f8de551ab37b
Orçun Aşık <asikorcun@gmail.com>
2023-06-25 20:25:13 +0300

    Created a new mig for CustomerAccountProcess class

--- EasyCash.DataAccess/Migrations/20230616112639_AddedDescriptionForCustomerAccountProcess.Designer.cs
+++ EasyCash.DataAccess/Migrations/20230616112639_AddedDescriptionForCustomerAccountProcess.Designer.cs
@@ -4,6 +4,7 @@ using EasyCash.DataAccess.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCash.DataAccess.Migrations
 {
     [DbContext(typeof(EasyCashDbContext))]
-    partial class EasyCashDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230616112639_AddedDescriptionForCustomerAccountProcess")]
+    partial class AddedDescriptionForCustomerAccountProcess
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EasyCash.DataAccess/Migrations/20230616112639_AddedDescriptionForCustomerAccountProcess.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCash.DataAccess.Migrations
+{
+    public partial class AddedDescriptionForCustomerAccountProcess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CustomerAccountProcesses");
+        }
+    }
+}
