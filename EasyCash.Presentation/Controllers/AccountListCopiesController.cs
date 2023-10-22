4e54a57b885d414a43b44759f7e2512f3a699652
Orçun Aşık <asikorcun@gmail.com>
2023-10-22 19:21:01 +0300

    Added AccountListCopiesController and View page

---
+++ EasyCash.Presentation/Controllers/AccountListCopiesController.cs
@@ -0,0 +1,25 @@
+﻿using EasyCash.Business.Abstract;
+using EasyCash.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCash.Presentation.Controllers
+{
+    public class AccountListCopiesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountService _customerAccountService;
+
+        public AccountListCopiesController(UserManager<AppUser> userManager, ICustomerAccountService customerAccountService)
+        {
+            _userManager = userManager;
+            _customerAccountService = customerAccountService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var datas = _customerAccountService.GetCustomerAccountList(user.Id);
+            return View(datas);
+        }
+    }
+}
